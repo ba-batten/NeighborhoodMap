2b363121995b40be6a98807b027a5f54c485c976
Brandon Batten <ba_batten@live.com>
2017-08-22 23:22:18 -0400

    Create map of Downtown Raleigh and apply styles

---
+++ js/maps.js
@@ -0,0 +1,96 @@
+function initMap() {
+  var raleigh = {lat: 35.779918, lng: -78.638523}
+  var map = new google.maps.Map(document.getElementById('map'),
+  {
+    center: raleigh,
+    zoom: 16,
+    styles: [
+      {
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#e1e1e1"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+          {
+            "color": "#13294b"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "landscape.man_made",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {
+            "color": "#4b9cd3"
+          }
+        ]
+      },
+      {
+        "featureType": "landscape.natural",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {
+            "color": "#4b9cd3"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {
+            "color": "#ffffff"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "labels.icon",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+          {
+            "color": "#13294b"
+          }
+        ]
+      },
+      {
+        "featureType": "transit",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      }
+    ]
+  });
+};
