819fe863d296368a2f3b91cf0a1d25b08bfcafc2
Brandon Batten <ba_batten@live.com>
2017-08-24 21:37:14 -0400

    Use a for loop to add markers to each location

--- js/maps.js
+++ js/maps.js
@@ -1,97 +1,18 @@
 function initMap() {
+  var data = new appViewModel
   var raleigh = {lat: 35.779918, lng: -78.638523}
   var map = new google.maps.Map(document.getElementById('map'),
   {
     center: raleigh,
-    zoom: 15,
-    // styles: [
-    //   {
-    //     "elementType": "labels.text.fill",
-    //     "stylers": [
-    //       {
-    //         "color": "#e1e1e1"
-    //       }
-    //     ]
-    //   },
-    //   {
-    //     "elementType": "labels.text.stroke",
-    //     "stylers": [
-    //       {
-    //         "color": "#13294b"
-    //       }
-    //     ]
-    //   },
-    //   {
-    //     "featureType": "administrative",
-    //     "elementType": "geometry",
-    //     "stylers": [
-    //       {
-    //         "visibility": "off"
-    //       }
-    //     ]
-    //   },
-    //   {
-    //     "featureType": "landscape.man_made",
-    //     "elementType": "geometry.fill",
-    //     "stylers": [
-    //       {
-    //         "color": "#4b9cd3"
-    //       }
-    //     ]
-    //   },
-    //   {
-    //     "featureType": "landscape.natural",
-    //     "elementType": "geometry.fill",
-    //     "stylers": [
-    //       {
-    //         "color": "#4b9cd3"
-    //       }
-    //     ]
-    //   },
-    //   {
-    //     "featureType": "poi",
-    //     "stylers": [
-    //       {
-    //         "visibility": "off"
-    //       }
-    //     ]
-    //   },
-    //   {
-    //     "featureType": "road",
-    //     "elementType": "geometry.fill",
-    //     "stylers": [
-    //       {
-    //         "color": "#ffffff"
-    //       }
-    //     ]
-    //   },
-    //   {
-    //     "featureType": "road",
-    //     "elementType": "labels.icon",
-    //     "stylers": [
-    //       {
-    //         "visibility": "off"
-    //       }
-    //     ]
-    //   },
-    //   {
-    //     "featureType": "road.highway",
-    //     "elementType": "geometry.stroke",
-    //     "stylers": [
-    //       {
-    //         "color": "#13294b"
-    //       }
-    //     ]
-    //   },
-    //   {
-    //     "featureType": "transit",
-    //     "stylers": [
-    //       {
-    //         "visibility": "off"
-    //       }
-    //     ]
-    //   }
-    // ]
-
+    zoom: 15
   });
+
+  for (var i = 0; i < data.locations.length; i++) {
+    var marker = new google.maps.Marker({
+      position: data.locations[i].coordinates,
+      map: map
+    });
+  }
+
+
 };
